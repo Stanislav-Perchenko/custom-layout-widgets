39add3960c6425aa46d64f4cdf9e88edf648a01e
stanislav.perchenko <lkjnbv123>
2018-10-22 17:40:05 +0300

    Initial commit

---
+++ app/src/main/java/com/alperez/samples/demoactivity/MultiChoiceViewDemoActivity.java
@@ -0,0 +1,14 @@
+package com.alperez.samples.demoactivity;
+
+import com.alperez.samples.R;
+
+/**
+ * Created by stanislav.perchenko on 10/22/2018
+ */
+public class MultiChoiceViewDemoActivity extends BaseDemoActivity {
+
+    @Override
+    protected int getLayoutResId() {
+        return R.layout.activity_demo_multi_choice_view;
+    }
+}
---
+++ widgets/src/main/java/com/alperez/widget/customlayout/DummyClass.java
@@ -0,0 +1,9 @@
+package com.alperez.widget.customlayout;
+
+/**
+ * Created by stanislav.perchenko on 10/22/2018
+ */
+public class DummyClass {
+
+    private DummyClass() { }
+}
