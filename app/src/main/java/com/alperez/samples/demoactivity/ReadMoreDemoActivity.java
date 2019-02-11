6acfe964782d414737995a74da55382db4267da0
stanislav.perchenko <lkjnbv123>
2019-02-11 17:53:43 +0200

    Developing TExtView with 'Read more'

--- app/src/main/java/com/alperez/samples/demoactivity/ReadMoreDemoActivity.java
+++ app/src/main/java/com/alperez/samples/demoactivity/ReadMoreDemoActivity.java
@@ -1,15 +1,47 @@
 package com.alperez.samples.demoactivity;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.ViewGroup;
+import android.widget.Switch;
+import android.widget.Toast;
+
 import com.alperez.samples.R;
+import com.alperez.widget.ReadMoreTextView;
 
 /**
  * Created by stanislav.perchenko on 2/8/2019
  */
 public class ReadMoreDemoActivity extends BaseDemoActivity {
 
+    private ReadMoreTextView vTxtReadMore;
 
     @Override
     protected int getLayoutResId() {
         return R.layout.activity_demo_read_more;
+    }
+
+    private CharSequence mReadMoreText;
+    private int marginLeft, marginRight;
+
+    @Override
+    protected void onPostCreate(@Nullable Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        vTxtReadMore = (ReadMoreTextView) findViewById(R.id.txt_read_more);
+        mReadMoreText = vTxtReadMore.getReadMoreText();
+        vTxtReadMore.setOnReadMoreClickListener(v -> Toast.makeText(this, "Click", Toast.LENGTH_SHORT).show());
+        ((Switch) findViewById(R.id.rm_en_switch)).setOnCheckedChangeListener((buttonView, isChecked) -> vTxtReadMore.setReadMoreText(isChecked ? mReadMoreText : null));
+        ((Switch) findViewById(R.id.size_switch)).setOnCheckedChangeListener((buttonView, isChecked) -> setViewMargin(isChecked ? 1 : 2));
+
+        ViewGroup.MarginLayoutParams mlp = (ViewGroup.MarginLayoutParams) vTxtReadMore.getLayoutParams();
+        marginLeft  = mlp.leftMargin;
+        marginRight = mlp.rightMargin;
+    }
+
+    private void setViewMargin(int scale) {
+        ViewGroup.MarginLayoutParams mlp = (ViewGroup.MarginLayoutParams) vTxtReadMore.getLayoutParams();
+        mlp.rightMargin = scale*marginRight;
+        mlp.leftMargin = scale*marginLeft;
+        vTxtReadMore.setLayoutParams(mlp);
     }
 }
--- widgets/src/main/java/com/alperez/widget/ReadMoreTextView.java
+++ widgets/src/main/java/com/alperez/widget/ReadMoreTextView.java
@@ -5,8 +5,15 @@ import android.content.Context;
 import android.content.res.TypedArray;
 import android.os.Build;
 import android.support.annotation.Nullable;
+import android.text.Layout;
+import android.text.Spannable;
+import android.text.SpannableString;
 import android.text.TextUtils;
+import android.text.style.ForegroundColorSpan;
+import android.text.style.StyleSpan;
 import android.util.AttributeSet;
+import android.util.Log;
+import android.view.MotionEvent;
 import android.widget.TextView;
 
 import com.alperez.widget.customlayout.R;
@@ -15,6 +22,10 @@ import com.alperez.widget.customlayout.R;
  * Created by stanislav.perchenko on 2/8/2019
  */
 public class ReadMoreTextView extends TextView {
+
+    public interface OnReadMoreClickListener {
+        void onReadMore(ReadMoreTextView v);
+    }
 
     public ReadMoreTextView(Context context) {
         super(context);
@@ -38,29 +49,153 @@ public class ReadMoreTextView extends TextView {
 
     private void extractAttrs(Context c, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
         TypedArray a = c.getResources().obtainAttributes(attrs, R.styleable.ReadMoreTextView);
-        //TODO extract attributes for the mOriginalReadMore and isReadMoreClickable
-        buildDisplayingReadMore(mOriginalReadMore);
+        mOriginalReadMore = a.getString(R.styleable.ReadMoreTextView_readMore_text);
+        isReadMoreClickable = a.getBoolean(R.styleable.ReadMoreTextView_readMore_clickable, false);
+        attrLoadMoreTextColor = a.getColor(R.styleable.ReadMoreTextView_readMore_textColor,0);
+        attrLoadMoreTextStyle = a.getInt(R.styleable.ReadMoreTextView_readMore_textStyle,0);
         a.recycle();
+
     }
 
-    private boolean isReadMoreEnabled;
+    private OnReadMoreClickListener onReadMoreClickListener;
+
     private boolean isReadMoreClickable;
-    private CharSequence mOriginalReadMore;
-    private CharSequence mDisplayReadMore;//This may be spanned
-    private boolean isLoadMoreDirty = true;
+    private String mOriginalReadMore;
+    private int attrLoadMoreTextColor;
+    private int attrLoadMoreTextStyle;
 
+    public void setOnReadMoreClickListener(@Nullable OnReadMoreClickListener l) {
+        this.onReadMoreClickListener = l;
+    }
 
-    public void setReadMoreText(CharSequence rm) {
+    private boolean isTextDirty;
+    private boolean isReadMoreShowing;
+
+    public void setReadMoreText(@Nullable CharSequence rm) {
         if (!TextUtils.equals(rm, mOriginalReadMore)) {
-            buildDisplayingReadMore(mOriginalReadMore = rm);
+            mOriginalReadMore = (rm == null) ? null : rm.toString();
+            if (isReadMoreShowing) {
+                isReadMoreShowing = false;
+                super.setText(mOrigText, (mOrigBuffType == null) ? BufferType.NORMAL : mOrigBuffType);
+            }
+            isTextDirty = true;
             requestLayout();
             invalidate();
         }
     }
 
-    private void buildDisplayingReadMore(CharSequence rm) {
-        //TODO Fill-in the mDisplayReadMore. Use isReadMoreClickable
+    @Override
+    public void setMaxLines(int maxLines) {
+        int oldLines = getMaxLines();
+        super.setMaxLines(maxLines);
+        if (getMaxLines() != oldLines) {
+            if (isReadMoreShowing) {
+                isReadMoreShowing = false;
+                super.setText(mOrigText, (mOrigBuffType == null) ? BufferType.NORMAL : mOrigBuffType);
+            }
+            isTextDirty = true;
+        }
+    }
 
-        isLoadMoreDirty = isReadMoreEnabled = !TextUtils.isEmpty(mDisplayReadMore);
+    private BufferType mOrigBuffType;
+    private CharSequence mOrigText;
+    @Override
+    public void setText(CharSequence text, BufferType type) {
+        isTextDirty = !TextUtils.equals(text, mOrigText) || (type != mOrigBuffType);
+        mOrigBuffType = type;
+        mOrigText = text;
+
+        if (isTextDirty || isReadMoreShowing) {
+            isReadMoreShowing = false;
+            super.setText(text, type);
+
+        }
+    }
+
+    public CharSequence getReadMoreText() {
+        return mOriginalReadMore;
+    }
+
+    private BufferType getOriginalBufferType() {
+        return (mOrigBuffType == null) ? BufferType.NORMAL : mOrigBuffType;
+    }
+
+
+
+
+
+
+
+
+
+
+    private int lastMeasureSpecW, lastMeasureSpecH;
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+
+        if (TextUtils.isEmpty(mOrigText)) return;
+
+        if ((lastMeasureSpecW != widthMeasureSpec) || (lastMeasureSpecH != heightMeasureSpec)) {
+            lastMeasureSpecW = widthMeasureSpec;
+            lastMeasureSpecH = heightMeasureSpec;
+            isTextDirty = true;
+            if (isReadMoreShowing) {
+                isReadMoreShowing = false;
+                post(() -> {
+                    super.setText(mOrigText, getOriginalBufferType());
+                    requestLayout();
+                    invalidate();
+                });
+                return;
+            }
+        }
+
+
+        if (!TextUtils.isEmpty(mOriginalReadMore) && isTextDirty && !isReadMoreShowing) {
+            final Layout layout = getLayout();
+            final int lastLineIndex = layout.getLineCount() - 1;
+            int start = layout.getLineStart(0);
+            int end = layout.getLineEnd(lastLineIndex);
+
+            String finReadMore = (mOriginalReadMore.charAt(0) == ' ') ? mOriginalReadMore : (" " + mOriginalReadMore);
+            if (getText().length() > (end - start)) {
+                final int contentWidth = getMeasuredWidth() - getPaddingLeft() - getPaddingRight();
+
+                final float readMoreWidth = getPaint().measureText(finReadMore);
+                final int targetMaxLastLine = contentWidth - (int) (1.12f * readMoreWidth);
+
+                final int lastLineStart = layout.getLineStart(lastLineIndex);
+                final int lastLineEnd = layout.getLineEnd(lastLineIndex);
+                String lastLine = getText().toString().substring(lastLineStart, lastLineEnd);
+
+                int finTextEnd = lastLineStart + lastLine.length() * targetMaxLastLine / contentWidth;
+
+                String finTextAndMore = getText().toString().substring(0, finTextEnd) + finReadMore;
+
+                Spannable spanText = new SpannableString(finTextAndMore);
+                spanText.setSpan(new StyleSpan(attrLoadMoreTextStyle), finTextEnd + 1, finTextAndMore.length(), Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
+                spanText.setSpan(new ForegroundColorSpan(attrLoadMoreTextColor), finTextEnd + 1, finTextAndMore.length(), Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
+
+                super.setText(spanText, getOriginalBufferType());
+                isReadMoreShowing = true;
+            }
+            isTextDirty = false;
+        }
+
+    }
+
+    @Override
+    protected void onLayout(boolean changed, int left, int top, int right, int bottom) {
+        if (changed) {
+            Log.e(getClass().getSimpleName(), String.format("onLayout() changed: l=%d, t=%d, r=%d, b=%d", left, top, right, bottom));
+        }
+        super.onLayout(changed, left, top, right, bottom);
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent ev) {
+        Log.d(getClass().getSimpleName(), ev.toString());
+        return super.onTouchEvent(ev);
     }
 }
